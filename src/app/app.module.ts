83dc3d08aaeee345ce1e86cfcf6a8df1bfbe50d7
Harshal Gite <harshal1402@gmail.com>
2019-06-08 22:39:46 +0530

    Component Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BannerComponent } from './banner/banner.component';
+import { SocialComponent } from './social/social.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ClientfeedbackComponent } from './clientfeedback/clientfeedback.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    BannerComponent,
+    SocialComponent,
+    AboutComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    ClientfeedbackComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
